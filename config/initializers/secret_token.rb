7654047203ff098b0c554565d8fd6912f7be3869
Adam MacLeod <adam@macleod.id.au>
2012-10-03 09:25:26 +1000

    Initialized new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Circled::Application.config.secret_token = '08f19e77462b62d612cbb631e27f2d30f854a9387681d65290d12b290defc24363e5d6d442280f5336212faba2f5be1be20855d3366b7f99ceadca99a8aba671'
