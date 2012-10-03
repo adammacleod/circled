c5ffe9c7381005bdf268a8786b41b87a65c40616
Adam MacLeod <adam@macleod.id.au>
2012-10-03 10:36:31 +1000

    Added some extra validation.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,8 @@
 class Link < ActiveRecord::Base
   attr_accessible :body, :link, :slug, :title
+
+  validates :link,  :presence => true
+  validates :slug,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
 end
