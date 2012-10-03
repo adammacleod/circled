3cb5d1b3ec6a26ff762ed5046ba374d0a91da1e2
Adam MacLeod <adam@macleod.id.au>
2012-10-04 08:38:14 +1000

    Made @current_user available to all views/controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :current_user
+
+  private
+
+  def logged_in?
+    !!session[:current_user_id]
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to logins_url
+    end
+  end
+
+  def current_user
+    if !@current_user and session[:current_user_id]
+      @current_user = User.find(session[:current_user_id])
+    end
+  end
 end
