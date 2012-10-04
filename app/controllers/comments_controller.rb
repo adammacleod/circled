a24ea68d4cdfbe78435245139d2149befcc69780
Adam MacLeod <adam@macleod.id.au>
2012-10-04 14:16:08 +1000

    Cleaned up comments controller, implemented display of comments via partials.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   # GET /links/:link_id/comments/id
   # GET /links/:link_id/comments/id.json
   def show
-    @link = Link.find_by_slug(params[:id])
+    @link = Link.find_by_slug(params[:link_id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -26,35 +26,35 @@ class CommentsController < ApplicationController
   # GET /links/:link_id/comments/new
   # GET /links/:link_id/comments/new.json
   def new
-    @link = Link.new
+    @link = Link.find_by_slug(params[:link_id])
+    @comment = @link.comments.create()
+    @comment.user = @current_user
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @link }
+      format.json { render json: @comment }
     end
   end
 
   # GET /links/:link_id/comments/id/edit
   def edit
-    @link = Link.find_by_id(params[:id])
-    unless @link.user == @current_user
-      redirect_to @link
-    end
+    redirect_to Comment.find_by_id(params[:id])
   end
 
   # POST /links/:link_id/comments
   # POST /links/:link_id/comments.json
   def create
-    @link = Link.new(params[:link])
-    @link.user = @current_user
+    @link = Link.find_by_slug(params[:link_id])
+    @comment = @link.comments.create(params[:comment])
+    @comment.user = @current_user
 
     respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render json: @link, status: :created, location: @link }
+      if @comment.save
+        format.html { redirect_to link_comments_url(@link), notice: 'Comment was created successfully' }
+        format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { render action: "new" }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
+        format.html { redirect_to link_comments_url(@link), error: 'Comment creation failed' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,32 +62,12 @@ class CommentsController < ApplicationController
   # PUT /links/:link_id/comments/id
   # PUT /links/:link_id/comments/id.json
   def update
-    @link = Link.find_by_id(params[:id])
-
-    respond_to do |format|
-      if @link.user != @current_user
-        flash[:error] = 'Unable to update links owned by other users.'
-        format.html { render action: "show" }
-        format.json { head :no_content }
-      elsif @link.update_attributes(params[:link])
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to Comment.find_by_id(params[:id])
   end
 
   # DELETE /links/:link_id/comments/id
   # DELETE /links/:link_id/comments/id.json
   def destroy
-    @link = Link.find_by_id(params[:id])
-    @link.destroy
-
-    respond_to do |format|
-      format.html { redirect_to links_url }
-      format.json { head :no_content }
-    end
+    redirect_to Comment.find_by_id(params[:id])
   end
 end
